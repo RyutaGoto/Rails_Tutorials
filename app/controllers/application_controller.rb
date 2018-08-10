b430e4bddfed6ffe667dae8057c1123820dcb2c2
RyutaGoto <kasa8gasa2rs6@gmail.com>
2018-08-10 18:44:56 +0900

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "¡Hola, mundo!"
-  end
-
-  def goobye
-    render html: "goodbye, world!"
-  end
-
-  def test
-    render html: "test text"
+    render html: "hello, world!!"
   end
 
 end
