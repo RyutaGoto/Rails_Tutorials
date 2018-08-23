3053aa49628cf830cce8b86842a9b885792d8446
RyutaGoto <kasa8gasa2rs6@gmail.com>
2018-08-23 19:38:00 +0900

    solved 5.1.1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
-    render html: "¡Hola, mundo!"
-  end
-
-  def goobye
-    render html: "goodbye, world!"
-  end
-
-  def test
-    render html: "test text"
-  end
+  
 
 end
